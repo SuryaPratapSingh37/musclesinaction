5fb0c2e4f06b478bdf149117a6c4f0dc420a051a
Surya Pratap Singh <70885700+SuryaPratapSingh37@users.noreply.github.com>
2023-09-29 17:47:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import setuptools
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    
 setuptools.setup(
     name="musclesinaction",
     version="0.0.1",
     author="Muhammed Kocabas",
     description="Muscles in Action",
     packages=setuptools.find_packages(),
+    install_reqs=required,
     python_requires='>=3.6',
 )
